f3ef93cfff5b96a2f8ab8cc20668802615d82b6f
Kureichi <hibikumonogatari@gmail.com>
2025-08-12 00:51:55 +0700

    add grab package for download manager

--- test.go
+++ test.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"github.com/Kuredew/GoMCLauncher/manager"
-	"github.com/Kuredew/GoMCLauncher/model"
+	"fmt"
+	"path/filepath"
+
+	"github.com/Kuredew/GoMCLauncher/utils"
 )
 
 func mains() {
-	instance := model.Instance{Name: "KureichiMinecraft", Version: "1.21.8", Modloader: "Fabric"}
-	
-	manager.StartInstance(instance)
+	hash := "b62ca8ec10d07e6bf5ac8dae0c8c1d2e6a1e3356"
+	hashId := string([]rune(hash)[:2])
+
+	url := "https://resources.download.minecraft.net/%s/%s"
+	downloadUrl := fmt.Sprintf(url, hashId, hash)
+
+	//fmt.Printf("%s\n", hashId)
+	//fmt.Printf("%s\n", downloadUrl)
+	utils.Download(filepath.Join("data", "downloadtest", hashId, hash), downloadUrl)
+
 }
---
+++ utils/download.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/cavaliergopher/grab/v3"
+)
+
+func Download(savedPath string, url string) bool {
+	client := grab.NewClient()
+	req, _ := grab.NewRequest(savedPath, url)
+
+	log.Printf("Downloading %v...\n", req.URL())
+	resp := client.Do(req)
+
+	// start UI loop
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+
+	Loop:
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("   Downloading %v / %v bytes (%.2f%%)\n", resp.BytesComplete(), resp.Size(), 100*resp.Progress())
+		case <-resp.Done:
+			break Loop
+		}
+
+	}
+	if err := resp.Err(); err != nil {
+		log.Fatalf("Downloading Failed. %v", err)
+	}
+	log.Printf("Download saved to %v", savedPath)
+
+	return true
+}
